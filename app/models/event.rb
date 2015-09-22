3eff9c3a3727d98f095d4916fe9daed29276533f
Luan Guimarães <luan@live.in>
2015-09-21 21:01:14 -0300

    Add some event field validations.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,17 @@
 class Event < ActiveRecord::Base
+	# Impossible to create an event without a name
+	validates(:name, presence: true)
+	validates(:name, length: {in: 10..40})
+
+	# Impossible to create an event without a local
+	validates(:local, presence: true)
+
+	# Impossible to create an event without a description text
+	validates(:description, presence: true)
+
+	# The description text must be bigger than 140 characters
+	# and shorter than 1000. These numbers were arbitrarily selected.
+	# subject to changes.
+	validates(:description, length: {in: 140..1000})
+
 end
