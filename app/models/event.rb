e0ce6969131c4b45cae50a254651ab8398a09cc1
Luan Guimarães <luan@live.in>
2015-09-21 23:24:11 -0300

    Add some test cases for the Event's fields.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,11 +6,15 @@ class Event < ActiveRecord::Base
 	# Impossible to create an event without a local
 	validates(:local, presence: true)
 
+	# The local string must be bigger than 6 characters and
+	# shorter than 41.
+	validates(:local, length: {in: 6..40})
+
 	# Impossible to create an event without a description text
 	validates(:description, presence: true)
 
 	# The description text must be bigger than 140 characters
-	# and shorter than 1000. These numbers were arbitrarily selected.
+	# and shorter than 1001. These numbers were arbitrarily selected.
 	# subject to changes.
 	validates(:description, length: {in: 140..1000})
 
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,15 +3,59 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
 
 	def setup
-		@event = Event.new(name: "Example name", local: "Example local", description: "Example description")
+		@event = Event.new(name: "Example name", local: "Example local", description: ("a" * 500))
 	end
 
 	test "should be valid" do
 		assert @event.valid?
 	end
 
-	test "name should be present" do
-		@event.name = "     "
+	# Name tests
+	test "event name should be present" do
+		@event.name = ""
 		assert_not @event.valid?
 	end
+
+	test "event name should not be too long" do
+		@event.name = "a" * 41
+		assert_not @event.valid?
+	end
+
+	test "event name should not be too short" do
+		@event.name = "b" * 9
+		assert_not @event.valid?
+	end
+
+	# Description tests
+	test "event description should be present" do
+		@event.description = ""
+		assert_not @event.valid?
+	end
+
+	test "event description should not be too long" do
+		@event.description = "a" * 1001
+		assert_not @event.valid?
+	end
+
+	test "event description should not be too short" do
+		@event.description = "b" * 139
+		assert_not @event.valid?
+	end
+
+	# Local tests
+	test "event local text should be present" do
+		@event.local = ""
+		assert_not @event.valid?
+	end
+
+	test "event local text should not be too short" do
+		@event.local = "a" * 5
+		assert_not @event.valid?
+	end
+
+	test "event local text should not be too long" do
+		@event.local = "a" * 41
+		assert_not @event.valid?
+	end
+
 end
