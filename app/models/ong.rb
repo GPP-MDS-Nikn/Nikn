fe91245b9a3b736b0dcc244ee5d56b2350259685
Miguel Nery <nery.mik@gmail.com>
2015-09-21 22:52:23 -0300

    Added some validation on the creation of new ongs

--- app/models/ong.rb
+++ app/models/ong.rb
@@ -1,2 +1,5 @@
 class Ong < ActiveRecord::Base
+	#This won't allow the creation of ONGs without names, or with names with lenght less than 3, or bigger than 40 characters
+	validates(:name, presence: true)
+	validates(:name, length: {in: 3..40})
 end
