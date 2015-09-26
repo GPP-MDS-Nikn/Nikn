a2fd4f5e8cd27a01b47b7af711bdb40ff65dd2b3
Luan Guimarães <luan@live.in>
2015-09-26 17:51:50 -0300

    Configuring routes to new event page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root          'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'new_event' => 'events#new'
+  resources :events
 
 
   # The priority is based upon order of creation: first created -> highest priority.
