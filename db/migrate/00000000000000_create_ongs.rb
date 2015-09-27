50c5c70f50ec281ecf7df9b27a9816d1bffefa9c
Miguel Nery <nery.mik@gmail.com>
2015-09-27 13:13:06 -0300

    Added more columns for Ongs on database

--- db/migrate/00000000000000_create_ongs.rb
+++ db/migrate/00000000000000_create_ongs.rb
@@ -2,9 +2,11 @@ class CreateOngs < ActiveRecord::Migration
   def change
     create_table :ongs do |t|
     #Creates ongs with the respective fields on database
-      t.text :name
+      t.integer :id
+      t.string :name
       t.text :description
       t.text :adress
+      t.string :email
       t.string :password_digest
 
       t.timestamps null: false
