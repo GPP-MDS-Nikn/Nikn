c5f84f6444efbb75a9e7649dd27ec2fa8b9bec42
duerno <felipeduernoal@gmail.com>
2015-09-27 11:45:00 -0300

    Add the feature of deleting and showing posts

--- app/controllers/portal_posts_controller.rb
+++ app/controllers/portal_posts_controller.rb
@@ -1,11 +1,15 @@
 class PortalPostsController < ApplicationController
-  before_action :set_portal_post, only: [:edit, :update, :destroy]
+  before_action :set_portal_post, only: [:show, :edit, :update, :destroy]
 
   # GET /portal_posts
   def index
     @portal_posts = PortalPost.all
 	end
   
+  # GET /portal_posts/<post:id>
+  def show
+  end
+
   # GET /portal_posts/new
   def new
     @portal_post = PortalPost.new
@@ -16,13 +20,23 @@ class PortalPostsController < ApplicationController
     @portal_post = PortalPost.new(portal_post_params)
     
     if @portal_post.save
-      redirect_to '/portal_posts', notice: 'Postagem criada com sucesso.'
+      redirect_to portal_posts_url, notice: 'Postagem criada com sucesso.'
     else
       render :new
     end
   end
   
+  # DELETE /portal_posts/<post:id>
+  def destroy
+    @portal_post.destroy
+    redirect_to portal_posts_url, notice: 'Postagem excluída com sucesso.'
+  end
+
   private 
+    def set_portal_post
+      @portal_post = PortalPost.find(params[:id])
+    end
+
     def portal_post_params
       params.require(:portal_post).permit(:author, :title, :body)
     end
