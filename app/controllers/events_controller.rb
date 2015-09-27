f22945950932d0accb4c7bc149e2875bda72f4d9
Luan Guimarães <luan@live.in>
2015-09-27 18:10:57 -0300

    Add some form validation structures.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,21 @@ class EventsController < ApplicationController
 	def new
 		@event = Event.new
 	end
+
+	def create
+		@event = Event.new(event_params)
+
+		if @event.save
+			# Criar uma página para autenticações com sucesso
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+		def event_params
+			return params.require(:event).permit(:name, :local, 
+								  :description, :date, :photo_path)
+		end
 end
