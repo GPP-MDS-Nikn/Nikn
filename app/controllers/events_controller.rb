a090529a08476613d75678cb7b96f5bf9ee2d094
Luan Guimarães <luan@live.in>
2015-09-27 19:53:04 -0300

    Add some important commentaries in Event Controller file.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
 	def show
+		# This is a useful way to make a individual page
+		# to each event registered at database
 		@event = Event.find(params[:id])
 	end
 
@@ -8,17 +10,27 @@ class EventsController < ApplicationController
 	end
 
 	def create
+		# Construct with permitted params
 		@event = Event.new(event_params)
 
 		if @event.save
-			# Criar uma página para autenticações com sucesso
+			# We could have used the equivalent
+			# redirect_to @event
+			redirect_to event_url(@event)
 		else
+			# If the form contains one or more erros,
+			# the alternative flux renders a new form page
+			# with some error indicators.
+			# This is implemented @ /app/views/shared/_error_messages.html.erb
 			render 'new'
 		end
 	end
 
 	private
 
+		# For security, we have to block the access to some
+		# parts of our db tables, so, Rails ( > 4.0) implements
+		# a way to describe which fields could be written.
 		def event_params
 			return params.require(:event).permit(:name, :local, 
 								  :description, :date, :photo_path)
