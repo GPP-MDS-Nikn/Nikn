7ec1054c0e41e70e56110947ab5ddf5bab3b001d
Rodrigo <rodrigo_77472669@hotmail.com>
2015-09-30 01:21:29 -0300

    Redefine input fields limit lengths

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,21 +6,21 @@ class Event < ActiveRecord::Base
 
 	# Impossible to create an event without a name
 	validates(:name, presence: true)
-	validates(:name, length: {in: 10..40})
+	validates(:name, length: {in: 3..60})
 
 	# Impossible to create an event without a local
 	validates(:local, presence: true)
 
 	# The local string must be bigger than 6 characters and
-	# shorter than 41.
-	validates(:local, length: {in: 6..40})
+	# shorter than 80.
+	validates(:local, length: {in: 6..80})
 
 	# Impossible to create an event without a description text
 	validates(:description, presence: true)
 
-	# The description text must be bigger than 140 characters
+	# The description text must be bigger than 20 characters
 	# and shorter than 1001. These numbers were arbitrarily selected.
 	# subject to changes.
-	validates(:description, length: {in: 140..1000})
+	validates(:description, length: {in: 20..1000})
 
 end
