f9f3b96c82ba6657bde48c9bbc5ec37221628c08
Miguel Nery <nery.mik@gmail.com>
2015-10-01 18:00:27 -0300

    Pages changed to Ongs

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -1,53 +1,7 @@
 class OngsController < ApplicationController
 
-  #Display ongs on database
 	def index
-    @ong = Ong.all
-  end
-  #Display individual ongs when selected
-  def show
-    @ong = Ong.find(params[:id])
-  end
-
-
-  #new and create shall add new ongs to database
-  def new
-  		@ong = Ong.new
-  	end
-  #..
-  def create
-    @ong = Ong.new(ong_params)
-    if @ong.save
-      session[:ong_id] = @ong.id
-      redirect_to '/ongs'
-    else
-      redirect_to '/ongs'
-    end
-  end
-  #edit ongs on database
-  def edit
-    @ong = Ong.find(params[:id])
-  end
-  #update database for edited ongs
-  def update
-    @ong = Ong.find(params[:id])
-      if @ong.update(ong_params)
-        redirect_to @ong
-      else
-        render 'edit'
-      end
-  end
-
-  def destroy
-    Ong.find(params[:id]).destroy
-    flash[:success] = "Ong deleted"
-    redirect_to ongs_url
-
-  end
-
-  	private
-#the private method ong_params determine what will be created from the fields on new.html.erb, the 'new' page.
-	def ong_params
-		params.require(:ong).permit(:name, :description, :adress, :password)
+		@ong = Ong.all
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'page#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
