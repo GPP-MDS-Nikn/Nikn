ca13d175b34cd2a64475e86396fc1ae4fc7dbf44
Miguel Nery <nery.mik@gmail.com>
2015-10-01 16:20:27 -0300

    Modified error messages and added some validation to fields

--- app/models/ong.rb
+++ app/models/ong.rb
@@ -3,4 +3,7 @@ class Ong < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
+  validates :name, :length => {:minimum => 2, :message => "must have at least 2 characters"}
+  validates :name, :uniqueness => {:message => "already taken"}
 end
