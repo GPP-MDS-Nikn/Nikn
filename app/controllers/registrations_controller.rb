023a27f51092d339eee00ea7ce42349b0a1f65e3
Miguel Nery <nery.mik@gmail.com>
2015-10-01 00:29:56 -0300

    Added fields 'name', 'adress' and 'description' to registration parameters

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,12 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation)
+  end
+end
--- db/migrate/00000000000001_add_fields_to_ongs.rb
+++ db/migrate/00000000000001_add_fields_to_ongs.rb
@@ -1,7 +1,7 @@
 class AddFieldsToOngs < ActiveRecord::Migration
 	def change
 	    add_column :ongs, :name, :string
-	    add_column :ongs, :descripiton, :text
+	    add_column :ongs, :description, :text
 	    add_column :ongs, :adress, :text
  	 end
 end
