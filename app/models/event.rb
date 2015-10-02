6d840453d1421f6211d340d2d9441e5019ece5c4
Luan Guimarães <luan@live.in>
2015-10-02 01:06:52 -0300

    Update Event show page.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
 	# Paperclip gem sintax for upload image files.
-	has_attached_file :image_file, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image_file, :styles => {}, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :image_file, :content_type => /\Aimage\/.*\Z/
 
 
