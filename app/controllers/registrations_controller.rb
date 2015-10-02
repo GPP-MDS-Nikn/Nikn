bdf9b4b197d8cbd0b7bff09493a2152b6c096c18
Miguel Nery <nery.mik@gmail.com>
2015-10-01 23:26:48 -0300

    Another edit bug fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation)
+    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation, :current_password)
   end
 
   def account_update_params
-    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation)
+    params.require(:ong).permit(:name, :description, :email, :adress, :password, :password_confirmation, :current_password)
   end
 end
