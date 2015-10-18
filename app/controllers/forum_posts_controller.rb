36b3fbac87a83f4a8848dc117979f6f0989a2078
LucasFreitas <lucas_but@hotmail.com>
2015-10-18 12:15:22 -0200

    Fixing forum posts controller

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,2 +1,21 @@
-class PostsController < ApplicationController
+class ForumPostsController < ApplicationController
+	
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+	end
+
+private 
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+
 end
