8670e47ee20cf2e12b20db0d685695ace75d03da
Luan Guimarães <luan@live.in>
2015-10-18 15:09:53 -0200

    Add Event model tests.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,59 +1,69 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Event do
-	before(:each) do
-		@event = create_event
-	end
-
 	context "when providing valid data" do
+		
+		# Valid tests
 		it "should be an instance of Event" do
-			expect(@event).to be_instance_of(Event)
+			event = create_event # Only valid data
+			expect(event).to be_instance_of(Event)
 		end
-
-		it "should be invalid without a title" do
-			@event.title = nil
-			expect(@event.valid?).to be false
-			expect(@event.errors[:title].size).to be > 0 # weird sintax to cat errors
-		end
-
 	end
 
-=begin
 	context "when providing invalid data" do
-	end
-	
-	it "should be valid" do
-		lambda {
-			event = create_event
-			event.expect be_valid
-			violated "#{event.errors.full_messages.to_sentence}" if event.new_record?
-		}.should change(Event, :count).by(1)
-	end
-
-	it "should be invalid without a title" do
-		lambda {
-			event = create_event(:title => nil)
-			event.errors.should be_invalid(:title)
-		}.should_not change(Event, :count)
-	end
-
-	it "should be invalid with short titles" do
-		lambda {
-			event = create_event(:title => "a" * (Event::TITLE_MIN_LENGTH - 1))
-			event.errors.should be_invalid(:title)
-		}.should_not change(Event, :count)
-	end
-
-	it "should be invalid with long titles" do
-		lambda {
-			event = create_event(:title => "a" * (Event::TITLE_MAX_LENGTH + 1))
-			event.errors.should be_invalid(:title)
-		}.should_not change(Event, :count)
+		
+		# Title tests
+		it "should be invalid without a title" do
+			event = create_event(title: nil)
+			expect(event.valid?).to be false
+			expect(event.errors[:title].size).to be > 0 # weird sintax to cat errors
 		end
-=end
+
+		it "should be invalid with short title" do
+			event = create_event(title: "a" * (Event::TITLE_MIN_LENGTH - 1))
+			expect(event.valid?).to be false
+		end
+
+		it "should be invalid with too long titles" do
+			event = create_event(title: "a" * (Event::TITLE_MAX_LENGTH + 1))
+			expect(event.valid?).to be false
+		end
+
+		#	Local tests
+		it "should be invalid without a local" do
+			event = create_event(local: nil)
+			expect(event.valid?).not_to be true
+		end
+
+		it "should be invalid with short local text" do
+			event = create_event(local: "a" * (Event::LOCAL_MIN_LENGTH - 1))
+			expect(event.valid?).not_to be true
+		end
+
+		it "should be invalid with too long local text" do
+			event = create_event(local: "a" * (Event::LOCAL_MAX_LENGTH + 1))
+			expect(event.valid?).not_to be true
+		end
+
+		# Description tests
+		it "should be invalid without a description" do
+			event = create_event(description: nil)
+			expect(event.valid?).to be false
+		end
+
+		it "should be invalid with short description" do
+			event = create_event(description: "a" * (Event::DESCRIPTION_MIN_LENGTH - 1))
+			expect(event.valid?).to be false
+		end
+
+		it "should be invalid with too long description" do
+			event = create_event(description: "a" * (Event::DESCRIPTION_MAX_LENGTH + 1))
+			expect(event.valid?).to be false
+		end
+
+	end
 
 	private
-
 		# Default settings to test
 		def create_event(options={})
 			Event.create({
