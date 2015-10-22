abba78afe1bc775ed4f3efe7689832c87f7c27c8
Miguel Nery <nery.mik@gmail.com>
2015-10-22 10:22:43 -0200

    Modified posts_controller for association

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,41 +1,48 @@
 class ForumPostsController < ApplicationController
-	before_action :find_forum_post, only: [:show, :edit, :update, :destroy]
+	#before_action :find_forum_post, only: [:show, :edit, :update, :destroy]
 	
-	def index
-		@forum_posts = ForumPost.all.order("created_at DESC")
-	end
-
-	def new
-		@forum_topic = ForumTopic.find(params[:forum_topic_id])
-		@forum_post = @forum_topic.forum_posts.create
-	end
+#	def index
+#		@forum_posts = ForumPost.all.order("created_at DESC")
+#	end
+#
+#	def new
+#		@forum_topic = ForumTopic.find(params[:forum_topic_id])
+#		@forum_post = @forum_topic.forum_posts.build
+#	end
 
 	def create
-		@forum_topic = ForumTopic.find(params[:forum_topic_id])
+		find_forum_topic
 		@forum_post = @forum_topic.forum_posts.create(forum_post_params)
 		@forum_post.save
 		if @forum_post.save
-			redirect_to @forum_post
+			redirect_to @forum_topic
 		else
 			render 'new'
 		end
 	end
 
 	def edit
+		find_forum_topic
+		@forum_post = @forum_topic.forum_posts.find(params[:id])
+
 	end
 
 	def update
-		if @forum_post.update(forum_post_params)	
-			redirect_to @forum_post
+		find_forum_topic
+		@forum_post = @forum_topic.forum_posts.find(params[:id])
+
+		if @forum_post.update(forum_post_params)
+			redirect_to @forum_topic
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
-		find_forum_post
+		find_forum_topic
+		@forum_post = @forum_topic.forum_posts.find(params[:id])
 		@forum_post.destroy
-		redirect_to root_path
+		redirect_to @forum_topic
 	end
 
 private 
@@ -44,9 +51,13 @@ private
 		@forum_post = ForumPost.find(params[:id])
 	end
 
+	def find_forum_topic
+		@forum_topic = ForumTopic.find(params[:forum_topic_id])
+	end
+
 
 	def forum_post_params
-		params.require(:forum_post).permit(:title, :content)
+		params.require(:forum_post).permit(:content)
 	end
 
 
