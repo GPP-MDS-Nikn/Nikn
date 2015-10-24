75e66f918c278fb44658dec6bb35cdb023a01a66
Luan Guimarães <luan@live.in>
2015-10-24 01:36:44 -0200

    Add method to fix and arrange each mortality rate region frequency to display it in a view chart.

--- app/controllers/mortality_rates_controller.rb
+++ app/controllers/mortality_rates_controller.rb
@@ -2,12 +2,17 @@ class MortalityRatesController < ApplicationController
   before_action :set_mortality_rate, only: [:show, :edit, :update, :destroy]
 
   def index
-    @mortality_rates = MortalityRate.all
+    @north_mortality_rates_frequencies = get_region_mortality_rate_frequency('Norte')
+    @northeast_mortality_rates_frequencies = get_region_mortality_rate_frequency('Nordeste')
+    @centerwest_mortality_rates_frequencies = get_region_mortality_rate_frequency('Centro-Oeste')
+    @south_mortality_rates_frequencies = get_region_mortality_rate_frequency('Sul')
+    @southeast_mortality_rates_frequencies = get_region_mortality_rate_frequency('Sudeste')
   end
 
   def new
     @mortality_rate = MortalityRate.new
   end
+
   def create
     @mortality_rate = MortalityRate.new(mortality_rate_params)
 
@@ -43,6 +48,70 @@ class MortalityRatesController < ApplicationController
   end
 
   private
+    def get_region_mortality_rate_frequency (region = nil)
+      if is_region_valid? region
+
+        logger.debug "Valid region: #{region}."
+
+        number_of_data_for_region = MortalityRate.where(regiao: region).count
+        
+        rate_ranges = [[0.0, 5.0],
+                       [5.0, 20.0],
+                       [20.0, 40.0],
+                       [40.0, 60.0],
+                       [60.0, 80.0],
+                       [80.0, 100.0]]
+
+        region_rates_frequency = []
+        rate_ranges.each do |each_range|
+          rates_per_range_counter = count_number_of_rates_in_limits(region, each_range[0], each_range[1])
+
+          rates_per_range_frequency = (rates_per_range_counter.to_f/number_of_data_for_region.to_f)*100.0
+
+          logger.debug "DEBUG: RANGE: #{each_range
+          }, NUMBER OF RATES: #{rates_per_range_counter
+          }, TOTAL RATES: #{number_of_data_for_region
+          }, FREQUENCY: #{rates_per_range_frequency}"
+
+          region_rates_frequency << rates_per_range_frequency
+        end
+
+        logger.debug "DEBUG: Array of frequecies per region #{region_rates_frequency}"
+        return region_rates_frequency
+      else
+        # [TODO] raise an exception for region name error
+      end
+    end
+
+    def count_number_of_rates_in_limits(region, limit_down, limit_up)
+      # This method takes on region as valid data 
+      if limit_down.class == Float && limit_up.class == Float
+        rates_in_limits_counter = 0
+
+      region_mortality_rate = MortalityRate.where(regiao: region)
+      region_mortality_rate.each do |each_rate| 
+          rate_value = each_rate.taxa.gsub(',','.').to_f
+
+          logger.debug "DEBUG: REGION: #{region}, NAME: #{each_rate.nome}, RATE(String): #{each_rate.taxa}, RATE(Float Converted): #{rate_value}."
+          if rate_value >= limit_down && rate_value < limit_up
+            rates_in_limits_counter += 1
+          else
+            # Do nothing and continue
+          end
+        end
+
+        logger.debug "DEBUG: Rates in the limits (#{limit_down}, #{limit_up}) | Got: #{rates_in_limits_counter}."
+        return rates_in_limits_counter
+      else
+        # [TODO] raise an exception for Float error
+      end
+    end
+
+    def is_region_valid?(region)
+      permitted_regions = ['Norte', 'Nordeste', 'Sul', 'Sudeste', 'Centro-Oeste']
+      return permitted_regions.include? region
+    end
+
     def set_mortality_rate
       @mortality_rate = MortalityRate.find(params[:id])
     end
