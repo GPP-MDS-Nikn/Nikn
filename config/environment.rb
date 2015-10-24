05322a05d30a1a72b71276932abccda9387252f6
Luan Guimarães <luan@live.in>
2015-10-24 01:34:52 -0200

    Add a logging layer to the Nikn project

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Initialize rails logger
+
+Rails.logger = Logger.new(STDOUT)
+config.log_level = :debug
