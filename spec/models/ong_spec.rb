695c7031d1d0505f7c46101495c0724f29646073
LucasFreitas <lucas_but@hotmail.com>
2015-10-26 10:39:15 -0200

    Fixing ong model spec

--- spec/models/ong_spec.rb
+++ spec/models/ong_spec.rb
@@ -1,26 +1,22 @@
-require 'rails_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-RSpec.describe Ong, type: :model do
-  
-  context "Creating a valid object and comparing parameters" do
-  	it "Should create a valid instance of object ONG" do
-  		ong = create_ong
-  		expect(ong.valid?).to be_instance_of(Ong)
-  	end
+describe Ong do
+    context "Creating a valid object and comparing parameters" do
+  	    
+        it "Should create a valid instance of object ONG" do
+  		    ong = create_ong
+  		    expect(ong.valid?).to be_instance_of(Ong)
+  	    end
+    end
 
+    context "Trying to create objetcs and validate with invalid data" do
 
-  end
+        it "shouldn't create a valid object with empty name" do
+    	    ong = create_ong(:name nil)
+    	    expect(ong.valid?).to be false
 
+        end
 
-  context "Trying to create objetcs and validate with invalid data" do
-
-  it "shouldn't create a valid object with empty name" do
-
-  	ong = create_ong(:name nil)
-  	expect(ong.valid?).to be false
-
-  end
-
-  end
+    end
 
 end
