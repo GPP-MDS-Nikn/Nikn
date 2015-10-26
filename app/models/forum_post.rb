c0e97e3c74a564ceaddf8aa6e7b194cb4ba351f9
LucasFreitas <lucas_but@hotmail.com>
2015-10-26 11:49:35 -0200

    Adding validation methods to Forum Posts model

--- app/models/forum_post.rb
+++ app/models/forum_post.rb
@@ -1,4 +1,11 @@
 class ForumPost < ActiveRecord::Base
 	belongs_to :forum_theme
 	belongs_to :forum_topic
+
+	validates(:title, presence: true)
+	validates(:title, length: {in: 3..30})
+
+	validates(:content, presence: true)
+	validates(:content, length: {in: 10..1000})
+
 end
