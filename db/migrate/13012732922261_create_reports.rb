a83752fdf42b3335a140adeceeaad5bba37ca7db
Miguel Nery <nery.mik@gmail.com>
2015-11-09 19:02:06 -0200

    Added admin controller, rmeoved reports migration

--- db/migrate/13012732922261_create_reports.rb
+++
@@ -1,9 +0,0 @@
-class CreateReports < ActiveRecord::Migration
-  def change
-    create_table :reports do |t|
-        t.string :content
-        t.integer :post_id
-        t.timestamps null: false
-    end
-  end
-end
