5a5dc660fbba8f6fda9ac96eb208f46dc25122a5
Miguel Nery <nery.mik@gmail.com>
2015-11-09 22:24:29 -0200

    Added index_reports in admin controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,3 +1,6 @@
 class AdminsController < ApplicationController
-    
+    def index_reports
+        @forum_posts = ForumPost.all
+        @reported_posts = @forum_posts.select { |post| post.reports >= 5 }
+    end
 end
