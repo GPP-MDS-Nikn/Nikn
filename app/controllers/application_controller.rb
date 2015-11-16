0d931300c42b89f39d5c0d24025d23c45b7de7fe
duerno <felipeduernoal@gmail.com>
2015-11-16 01:39:59 -0200

    Set alias to say current_user = current_ong, and handle 'access denied' exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
   
   # Check authorization for every action, unless its made by a devise model
   check_authorization :unless => :devise_controller?
+
+  # CanCan expects a current_user method from your controller. 
+  # Then, I need to set an alias to say current_user = current_ong.
+  def current_ability
+  	@current_ability ||= Ability.new(current_ong)
+	end
+
+	# Handle 'access denied' exception redirecting the user to the home page
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :notice => exception.message
+  end
 end
