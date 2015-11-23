3a3ba7145be7073eb74312ac2a973e0ffb888d74
duerno <felipeduernoal@gmail.com>
2015-11-23 11:22:46 -0200

    Correct indent and improve some comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,42 +1,20 @@
 class Ability
-    include CanCan::Ability
+	include CanCan::Ability
 
-    def initialize(user)
-        user ||= Ong.new # guest user (not logged in)
-        if user.has_role? :admin
-            can :manage, [ForumTheme, ForumTopic, ForumPost, Event, PortalPost, MortalityRate]
-        elsif user.has_role? :ong
-            can :manage, [ForumTopic, Event, PortalPost], :ong_id => user.id
-            can :create, [ForumTopic, Event, PortalPost]
-        end
-        can :create, ForumPost
-        can :read, [ForumTheme, ForumTopic, ForumPost, Event, PortalPost, MortalityRate]
-    end
+	def initialize(user)
+		# If the user parameter is not passed, initialize it as a guest user (not logged in)	
+		user ||= Ong.new
+		
+		if user.has_role? :admin
+			can :manage, [ForumTheme, ForumTopic, ForumPost, Event, PortalPost, MortalityRate]
+		
+		elsif user.has_role? :ong
+			can :manage, [ForumTopic, Event, PortalPost], :ong_id => user.id
+			can :create, [ForumTopic, Event, PortalPost]
+		end
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+		can :create, ForumPost
+		can :read, [ForumTheme, ForumTopic, ForumPost, Event, PortalPost, MortalityRate]
+	end
+
 end
--- spec/models/care_unit_spec.rb
+++ spec/models/care_unit_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe CareUnit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe CareUnit, type: :model do
+	pending "add some examples to (or delete) #{__FILE__}"
 end
