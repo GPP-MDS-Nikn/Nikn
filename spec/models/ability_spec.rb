065e5209c543b1ad9094e5da8993cf31177362f7
duerno <felipeduernoal@gmail.com>
2015-11-23 11:39:06 -0200

    Add rspec tester for cancan model 'Ability'

---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,10 @@
+# This file includes tests for access restriction. 
+# The Ability model (from CanCanCan gem) is used to define abilities of users based on their roles. 
+# So, this tester basically tests the access restriction of users to any route and resource they may try to access.
+
+require "cancan/matchers"
+require 'rails_helper'
+
+describe Ability do
+	pending "add some examples to 'ability_spec.rb'"
+end
