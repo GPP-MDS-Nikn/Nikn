dcc79e3c81571ed2c3d10413f8673f6fe3783fbe
Luan Guimarães <luan@live.in>
2015-11-24 01:28:03 -0200

    Move the method to group lists and improve a new database search method in the ForumTheme controller.

--- app/controllers/forum_themes_controller.rb
+++ app/controllers/forum_themes_controller.rb
@@ -1,56 +1,68 @@
 class ForumThemesController < ApplicationController
-	def index
-		@forum_theme = ForumTheme.all
-	end
-	
-	def show 
-	  	set_forum_theme
-	  	@forum_topics = @forum_theme.forum_topics
-	end
+  include ApplicationHelper
 
-	def new
-		@forum_theme = ForumTheme.new
-	end
+  def index
+    @forum_theme = ForumTheme.all
 
-	def create
-		@forum_theme = ForumTheme.new(forum_theme_params)
+    # Get last forum topics
+    ungrouped_last_forum_topics = get_last_forum_topics(9)
+    # Group the last forum topics in specified sub-lists.
+    # To see the method structure, go to the application helper.
+    @grouped_last_forum_topics = create_list_groups(ungrouped_last_forum_topics, 3)
+  end
 
-		if @forum_theme.save
-			redirect_to '/forum_themes'
-		else
-			render 'new'
-		end
-	end
+  def show 
+    set_forum_theme
+    @forum_topics = @forum_theme.forum_topics
+  end
 
-	def edit
-		set_forum_theme
-	end
+  def new
+    @forum_theme = ForumTheme.new
+  end
 
-	def update
-		set_forum_theme
-	    if @forum_theme.update(forum_theme_params)
-	      flash[:success] = "Tema atualizado!"
+  def create
+    @forum_theme = ForumTheme.new(forum_theme_params)
 
-	      redirect_to @forum_theme
-	    else
-	      render :edit
-	    end
-  	end
+    if @forum_theme.save
+        redirect_to '/forum_themes'
+    else
+        render 'new'
+    end
+  end
 
-	def destroy 
-		set_forum_theme
-		@forum_theme.destroy
-		redirect_to forum_themes_path
-	end
+  def edit
+    set_forum_theme
+  end
 
-	private
+  def update
+    set_forum_theme
+    if @forum_theme.update(forum_theme_params)
+      flash[:success] = "Tema atualizado!"
 
-	 def set_forum_theme
+      redirect_to @forum_theme
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    set_forum_theme
+    @forum_theme.destroy
+    redirect_to forum_themes_path
+  end
+
+  private
+    # Method to get some forum topics
+    def get_last_forum_topics(quantity)
+      return ForumTopic.order(created_at: :desc).limit(quantity)
+    end
+
+    # Method to take a ForumTheme with a specified identifier
+    def set_forum_theme
       @forum_theme = ForumTheme.find(params[:id])
     end
 
-  	def forum_theme_params
-    	params.require(:forum_theme).permit(:title, :description)
-  	end
-
+    def forum_theme_params
+        params.require(:forum_theme).permit(:title, :description)
+    end
 end
