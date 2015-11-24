b5b9dd1da3066e71ae1341a15d15975acdacac20
Luan Guimarães <luan@live.in>
2015-11-24 06:38:30 -0200

    Redefine the ForumTheme archives to be more readable.

--- app/controllers/forum_themes_controller.rb
+++ app/controllers/forum_themes_controller.rb
@@ -4,13 +4,13 @@ class ForumThemesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @forum_theme = ForumTheme.all
 
     # Get last forum topics
     ungrouped_last_forum_topics = get_last_forum_topics(9)
     # Group the last forum topics in specified sub-lists.
     # To see the method structure, go to the application helper.
     @grouped_last_forum_topics = create_list_groups(ungrouped_last_forum_topics, 3)
+    @forum_themes = ForumTheme.all
   end
 
   def show
