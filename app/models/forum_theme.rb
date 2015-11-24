be5b8e2d51bea22d6232e103b7c8866c18c3b2b8
Luan Guimarães <luan@live.in>
2015-11-24 01:38:42 -0200

    Improve image file field to the ForumTheme.

--- app/models/forum_theme.rb
+++ app/models/forum_theme.rb
@@ -1,10 +1,12 @@
 class ForumTheme< ActiveRecord::Base
 	has_many :forum_topics, dependent: :destroy
 
+    # Paperclip gem sintax for upload image files.
+	has_attached_file :image_file, :styles => {}, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :image_file, :content_type => /\Aimage\/.*\Z/
+
 	TITLE_MIN_LENGTH = 2
 	DESCRIPTION_MIN_LENGTH = 2
-
 	validates :title, presence: true, :length => {:minimum => TITLE_MIN_LENGTH}, uniqueness: true
 	validates :description, presence: true, :length => {:minimum => DESCRIPTION_MIN_LENGTH}
-
 end
