af9f739ce9e6b9f02a13583a3ba76072a047ff33
duerno <felipeduernoal@gmail.com>
2015-11-26 04:29:44 -0200

    Add tests for when using empty or too small/large data in ong tester

--- spec/models/ong_spec.rb
+++ spec/models/ong_spec.rb
@@ -42,6 +42,129 @@ describe Ong do
 		end
 	end
 
+	# Must not create ongs with empty (if the presence is needed) or too small string attributes
+	context "when using empty or too small data" do
+		it "shouldn't create an object with an empty name" do
+			ong = create_ong
+			ong.name = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a name too small" do
+			ong = create_ong
+			ong.name = "a"*(NAME_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty description" do
+			ong = create_ong
+			ong.description = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a description too small" do
+			ong = create_ong
+			ong.description = "a"*(DESCRIPTION_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a site too small" do
+			ong = create_ong
+			ong.site = "a"*(SITE_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty phone" do
+			ong = create_ong
+			ong.phone = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a phone too small" do
+			ong = create_ong
+			ong.phone = "1"*(PHONES_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a phone too large" do
+			ong = create_ong
+			ong.phone = "1"*(PHONES_MAX_LENGTH + 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an auxiliary phone too small" do
+			ong = create_ong
+			ong.auxiliary_phone = "1"*(PHONES_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an auxiliary phone too large" do
+			ong = create_ong
+			ong.auxiliary_phone = "1"*(PHONES_MAX_LENGTH + 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty cep" do
+			ong = create_ong
+			ong.cep = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a cep too small" do
+			ong = create_ong
+			ong.cep = "1"*(CEP_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a cep too large" do
+			ong = create_ong
+			ong.cep = "1"*(CEP_LENGTH + 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty address" do
+			ong = create_ong
+			ong.address = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an address too small" do
+			ong = create_ong
+			ong.address = "a"*(ADDRESS_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty uf" do
+			ong = create_ong
+			ong.uf = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an uf too small" do
+			ong = create_ong
+			ong.uf = "a"*(UF_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an uf too large" do
+			ong = create_ong
+			ong.uf = "a"*(UF_LENGTH + 1)
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with an empty city" do
+			ong = create_ong
+			ong.city = nil
+			expect(ong.valid?).to be false
+		end
+
+		it "shouldn't create an object with a city too small" do
+			ong = create_ong
+			ong.city = "a"*(CITY_MIN_LENGTH - 1)
+			expect(ong.valid?).to be false
+		end
+	end
+
 	private
 		# Ong constants (in characters)
 		NAME_MIN_LENGTH 				= 8
