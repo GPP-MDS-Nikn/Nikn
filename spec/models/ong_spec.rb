26ceb2da4eaa2551923eda98c5af39d2b05bdbec
duerno <felipeduernoal@gmail.com>
2015-11-26 04:27:32 -0200

    Add function to create a valid ong and test creation of valid ongs in the ong model tester file

--- spec/models/ong_spec.rb
+++ spec/models/ong_spec.rb
@@ -1,31 +1,41 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'rails_helper'
 
 describe Ong do
-    context "Creating a valid object and comparing parameters" do
+	context "when using valid data" do
+		it "should create a valid instance of object ONG" do
+			ong = create_ong("1")
+			expect(ong.valid?).to be true
+		end
+	end
 
-        it "Should create a valid instance of object ONG" do
-  		    ong = create_ong
-  		    expect(ong.valid?).to be true
-  	    end
-    end
+	private
+		# Ong constants (in characters)
+		NAME_MIN_LENGTH 				= 8
+		DESCRIPTION_MIN_LENGTH 	= 100
+		SITE_MIN_LENGTH 				= 5
+		PHONES_MIN_LENGTH 			= 10
+		PHONES_MAX_LENGTH 			= 11
+		CEP_LENGTH 							= 8
+		ADDRESS_MIN_LENGTH 			= 10
+		UF_LENGTH 							= 2
+		CITY_MIN_LENGTH 				= 3
 
-    context "Trying to create objetcs and validate with invalid data" do
-
-        it "shouldn't create a valid object with empty email" do
-    	    ong = create_ong(email: nil)
-    	    expect(ong.valid?).to be false
-
-        end
-
-    end
-
-    private
-
-    def create_ong(options={})
-      Ong.create({
-        email: "ong_test@email.com",
-        password: "12345678"
-      }.merge(options))
-    end
+		# Default settings to test, only valid fields.
+		def create_ong(id="1", options={})
+			Ong.create({
+			email: "ong" + id + "@example.com",
+			password: "password",
+			password_confirmation: "password",
+			name: "Ong" + id + " name example",
+			description: ("Description " + id + " example")*10,
+			site: "www.example" + id + ".com",
+			phone: "613333222" + id,
+			auxiliary_phone: "613333222" + id,
+			cep: id + "2345678",
+			address: "example address " + id,
+			uf: "DF",
+			city: "Brasilia"
+			}.merge(options))
+		end
 
 end
