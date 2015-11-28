e42cd67ddf498098d06fee26df8dfc753dc017bc
duerno <felipeduernoal@gmail.com>
2015-11-28 19:18:59 -0200

    Permit additional parameters to get linked with ong instances in signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
+  # Strong parameters for ongs create/update.
+  # Ths 'before_action' ensures a link between ongs and their custom attributes
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   # Check authorization for every action, unless its made by a devise model
   check_authorization :unless => :devise_controller?
 
-  # CanCan expects a current_user method from your controller. 
+  # CanCan expects a current_user method from your controller.
   # Then, I need to set an alias to say current_user = current_ong.
   def current_ability
   	@current_ability ||= Ability.new(current_ong)
@@ -16,4 +19,9 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :notice => exception.message
   end
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation,
+				:name, :description, :image_file, :site, :phone, :auxiliary_phone, :cep, :address, :complement, :uf, :city) }
+		end
 end
