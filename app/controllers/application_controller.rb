af3480066d0ec9f0c9e229d4ab96e888b1e4d296
duerno <felipeduernoal@gmail.com>
2015-11-28 19:46:18 -0200

    Fix some links and the ongs/show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   # Check authorization for every action, unless its made by a devise model
   check_authorization :unless => :devise_controller?
 
