cf3883b04febcb60169b06f88c080fdf5faea620
duerno <felipeduernoal@gmail.com>
2015-11-28 19:18:59 -0200

    Permit additional parameters to get linked with ong instances in signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	# Strong parameters for ongs create/update.
+	# Ths 'before_action' ensures a link between ongs and their custom attributes
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, 
+				:name, :description, :image_file, :site, :phone, :auxiliary_phone, :cep, :address, :complement, :uf, :city) }
+		end
 end
