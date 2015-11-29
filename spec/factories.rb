ed77e543e0762ad449b12b05b7976a17437363d8
Miguel Nery <nery.mik@gmail.com>
2015-11-29 02:12:49 -0200

    forum_topic tests covered

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,12 +1,18 @@
 FactoryGirl.define do
-  factory :forum_theme do
-    title "theme title"
-    description  "theme description"
-  end
+    factory :forum_theme do
+        title "theme title"
+        description  "theme description"
+    end
 
- factory :forum_topic do
-    title "topic title"
-    body "topic body"
-    forum_theme
-  end
+    factory :forum_topic do
+        title "topic title"
+        body "topic body"
+        author "author"
+        forum_theme
+    end
+
+    factory :forum_post do
+        content "post content"
+        forum_topic
+    end
 end
