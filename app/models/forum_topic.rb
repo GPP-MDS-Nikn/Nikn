4ae94c0b0dee6bf556c0d5d00f8f377dd35f784f
Miguel Nery <nery.mik@gmail.com>
2015-11-29 00:00:23 -0200

    fixed association on forum topics with themes

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -6,6 +6,7 @@ class ForumTopic < ActiveRecord::Base
 	BODY_MIN_LENGTH = 2
 
 	has_many :forum_posts, dependent: :destroy
+	belongs_to :forum_theme
 
 	validates :title, presence: true, :length => {:minimum => TITLE_MIN_LENGTH}, uniqueness: true
 	validates :body, presence: true, :length => {:minimum => BODY_MIN_LENGTH}
