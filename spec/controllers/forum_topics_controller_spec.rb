dd33dc4b7392f7414be7344eb862ba9f90b72a8b
Miguel Nery <nery.mik@gmail.com>
2015-11-29 02:12:49 -0200

    forum_topic tests covered

--- spec/controllers/forum_topics_controller_spec.rb
+++ spec/controllers/forum_topics_controller_spec.rb
@@ -2,69 +2,113 @@ require 'rails_helper'
 
 describe ForumTopicsController, :type => :controller do
     context 'creating topics' do
+
+        it 'should build new topic from existing theme' do
+            @theme = create(:forum_theme)
+            get :new, :forum_theme_id => @theme.id
+            expect(assigns(:forum_topic).forum_theme_id).to eq(@theme.id)
+        end
+
         it 'should create topic with valid params' do
             @theme = create(:forum_theme)
             @topic_attributes = FactoryGirl.attributes_for(:forum_topic, :forum_theme_id => @theme)
             expect{(
 				post :create, :forum_theme_id => @theme, :forum_topic => @topic_attributes
 				)}.to change(ForumTopic, :count).by(1)
-    #        expect{create_topic}.to change(ForumTopic, :count).by(1)
         end
         it 'should not create topic with invalid params' do
-            expect{create_topic(title: nil)}.to change(ForumTopic, :count).by(0)
-        end
-
-        it 'initializes with 0 reports' do
-            @topic = create_topic
-            expect(@topic.reports).to eq(0)
+            @theme = create(:forum_theme)
+            @topic_attributes = FactoryGirl.attributes_for(:forum_topic, :forum_theme_id => @theme, title: nil)
+            expect{(
+				post :create, :forum_theme_id => @theme, :forum_topic => @topic_attributes
+				)}.to change(ForumTopic, :count).by(0)
         end
     end
-    describe 'PUT #update' do
-    #context 'updating topics' do
-        it 'should update existing topic' do
-            @topic = create_topic
-            update_topic(@topic)
-            expect(@topic.title).to eq('new title')
+
+    context 'showing topics' do
+        it 'should show existing topic' do
+            @topic = create(:forum_topic)
+            get :show, id: @topic.id
+            expect(assigns(:forum_topic)).to eq(@topic)
+        end
+    end
+
+    context 'updating topics' do
+
+        it 'should assign right topic to editing' do
+            @topic = create(:forum_topic)
+            get :edit, id: @topic.id
+            expect(assigns(:forum_topic)).to eq(@topic)
+        end
+        it 'should update existing topic with valid params' do
+            @topic = create(:forum_topic)
+            @new_attributes = {title: 'new_title'}
+            put :update, :id => @topic.id, :forum_topic => @new_attributes
+            @topic.reload
+            expect(@topic.title).to eq('new_title')
+        end
+
+        it 'should not update existing topic with invalid params' do
+            @topic = create(:forum_topic)
+            @new_attributes = {title: nil}
+            put :update, :id => @topic.id, :forum_topic => @new_attributes
+            @topic.reload
+            expect(@topic.title).to eq('topic title')
         end
     end
 
     context 'deleting topics' do
         it 'should delete existing topic from database' do
-            @topic = create_topic
-            expect{@topic.destroy}.to change(ForumTopic, :count).by(-1)
+            @topic = create(:forum_topic)
+            expect{delete :destroy, :forum_theme_id => @topic.forum_theme_id, :id => @topic.id}.to change(ForumTopic, :count).by(-1)
         end
     end
 
+    context  'reporting topics' do
+        it 'should assign right topic to be reported' do
+            @topic = create(:forum_topic)
+            get :new_report_topic, :id => @topic.id
+            expect(assigns(:forum_topic)).to eq(@topic)
+        end
+    end
+        it 'should redirect to report page if captcha is correct (topic)' do
+            @topic = create(:forum_topic)
+            verify_recaptcha = true
+            patch :create_report_topic, :id => @topic.id
+            expect(response).to redirect_to(new_report_topic_path)
+        end
 
-private
-    def create_topic(options={})
-        ForumTopic.create({
-            title: 'title',
-            body: 'body'
-        }.merge(options))
+        it 'should not redirect to report page if captcha isnt correct (topic)' do
+            @post = create(:forum_post)
+            verify_recaptcha = false
+            patch :create_report_post, :id => @post.id
+            expect(response).to redirect_to(new_report_post_path)
+        end
+
+    context 'reporting posts' do
+        it 'should assign right post to be reported' do
+            @post = create(:forum_post)
+            get :new_report_post, :id => @post.id
+            expect(assigns(:forum_post)).to eq(@post)
+        end
+
+        it 'should redirect to report page if captcha is correct (post)' do
+            @post = create(:forum_post)
+            verify_recaptcha = true
+            patch :create_report_post, :id => @post.id
+            expect(response).to redirect_to(new_report_post_path)
+        end
+
+        it 'should redirect to report page if captcha isnt correct (post)' do
+            @post = create(:forum_post)
+            verify_recaptcha = false
+            patch :create_report_post, :id => @post.id
+            expect(response).to redirect_to(new_report_post_path)
+        end
     end
 
-    def create_theme(options={})
-        ForumTheme.create({
-            title: 'title',
-            description: 'description'
-        }.merge(options))
+    context 'reporting posts' do
 
     end
-
-    def update_topic(topic)
-        topic.update(
-            title: 'new title',
-            body: 'new body'
-        )
-    end
-
-    def some_params(options={})
-        return ({
-            title: "Event's name",
-            body: 'body'
-        }.merge(options))
-    end
-
 
 end
