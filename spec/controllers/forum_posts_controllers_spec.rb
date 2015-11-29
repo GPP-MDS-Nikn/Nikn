07e0ade0fa7a6d5bf3d51176e09bba2c8eff0059
Miguel Nery <nery.mik@gmail.com>
2015-11-29 21:14:14 -0200

    Added unit tests for forum_posts

--- spec/controllers/forum_posts_controllers_spec.rb
+++ spec/controllers/forum_posts_controllers_spec.rb
@@ -3,28 +3,20 @@ require 'rails_helper'
 describe ForumPostsController, :type => :controller do
     context "creating a new post" do
         it "saves a new post" do
-            expect{create_post}.to change(ForumPost, :count).by(1)
+            @topic = create(:forum_topic)
+            @post_attrs = attributes_for(:forum_post, :forum_topic_id => @topic.id)
+            expect{
+                post :create, :forum_topic_id => @topic, :forum_post => @post_attrs
+            }.to change(ForumPost, :count).by(1)
         end
 
         it "initialize with 0 reports" do
-            @post = create_post
-            expect(@post.reports).to eq(0)
+
         end
 
         it "denies creation of bad post" do
-            expect{create_post(content: "a")}.to change(ForumPost, :count).by(0)
+        
         end
 
     end
-
-
-private
-    # Default settings to test.
-    # Only valid fields.
-    def create_post(options={})
-        ForumPost.create({
-            content: "content"
-        }.merge(options))
-    end
-
 end
