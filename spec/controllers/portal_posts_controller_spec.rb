89d86523a99c0e67de5257424e236c32dc18c07b
Miguel Nery <nery.mik@gmail.com>
2015-11-29 11:34:37 -0200

    Modified tests for portal posts

--- spec/controllers/portal_posts_controller_spec.rb
+++ spec/controllers/portal_posts_controller_spec.rb
@@ -3,45 +3,58 @@ require 'rails_helper'
 describe PortalPostsController, :type => :controller do
     context 'creating posts' do
         it 'should create a post with valid params' do
-            expect {@post = create_portal_post}.to change(PortalPost, :count).by(1)
-
+            @post_attributes = attributes_for(:portal_post)
+            expect{
+                post :create, :portal_post => @post_attributes
+            }.to change(PortalPost, :count).by(1)
         end
 
         it 'should not create a post with invalid params' do
-            expect {@post = create_portal_post(author: nil)}.to change(PortalPost, :count).by(0)
+            @post_attributes = attributes_for(:portal_post, title: nil)
+            expect{
+                post :create, :portal_post => @post_attributes
+            }.to change(PortalPost, :count).by(0)
+        end
+    end
 
+    context 'showing posts' do
+        it 'should show a post' do
+            create_post
+            get :show, :id => @post.id
+            expect(assigns(:portal_post)).to eq(@post)
+
+        end
+    end
+
+    context 'updating posts' do
+        it 'should update existing post with valid params' do
+            create_post
+            @attrs = {title: 'new title'}
+            put :update, :id =>@post.id, :portal_post => @attrs
+            @post.reload
+            expect(@post.title).to eq('new title')
+        end
+
+        it 'should not update existing post with invalid params' do
+            create_post
+            @attrs = {title: nil}
+            put :update, :id =>@post.id, :portal_post => @attrs
+            @post.reload
+            expect(@post.title).to eq('portal_post title')
         end
     end
 
     context 'deleting posts' do
         it 'should delete existing post from database' do
-            @post = create_portal_post
-            expect {@post.destroy}.to change(PortalPost, :count).by(-1)
-        end
-    end
-
-    context 'updating posts' do
-        it 'should update fields in posts' do
-            @post = create_portal_post
-            update_portal_post(@post)
-            expect(@post.author).to eq("new author")
+            create_post
+            expect{
+                delete :destroy, :id => @post.id
+            }.to change(PortalPost, :count).by(-1)
         end
     end
 
 private
-    def create_portal_post(options={})
-        PortalPost.create({
-            author: "author",
-            title: "title",
-            body: "body"
-        }.merge(options))
-    end
-
-    def update_portal_post(post)
-        post.update(
-            author: "new author",
-            title: "new title",
-            body: "new body"
-        )
+    def create_post
+        @post = create(:portal_post)
     end
 end
