ae7a55fd8b966f6fad94b5f9917e64ab4145dc96
Luan Guimarães <luan@live.in>
2015-11-30 01:37:06 -0200

    Add pagination funcionality to the ForumThemesController.

--- app/controllers/forum_themes_controller.rb
+++ app/controllers/forum_themes_controller.rb
@@ -2,6 +2,7 @@ class ForumThemesController < ApplicationController
   include ApplicationHelper
 	# Use this controller as a resource of CanCan and Rolify
   load_and_authorize_resource
+  PAGINATION_RANGE = 5
 
   def index
     # Get last forum topics
@@ -13,13 +14,21 @@ class ForumThemesController < ApplicationController
   end
 
   def show
+
     set_forum_theme
     # If the user is searching for something, returns the result of the research
     if params[:search]
-      @forum_topics = @forum_theme.forum_topics.search(params[:search]).order("created_at DESC")
+      @forum_topics = @forum_theme.forum_topics.search(params[:search]).order("created_at DESC").paginate(page: params[:page], per_page: PAGINATION_RANGE)
     else
-      @forum_topics = @forum_theme.forum_topics.all.order('created_at DESC')
+      @forum_topics = @forum_theme.forum_topics.all.order('created_at DESC').paginate(page: params[:page], per_page: PAGINATION_RANGE)
     end
+    number_of_pages = (@forum_topics.count.to_f / PAGINATION_RANGE.to_f).ceil
+    @pages = 1..number_of_pages
+    @page = params[:page]
+    @page ||= 1
+    logger.debug "Quantity of paginated topics: #{ @forum_topics.count }."
+    logger.debug "Number of pages: #{ number_of_pages }."
+    logger.debug "Current page: #{ @page }."
   end
 
   def new
