e5731d4aff920a8b9d778731d7047e0511d7957a
Luan Guimarães <luan@live.in>
2015-11-29 23:31:40 -0200

    Fix some errors when trying to redirect to invalid path.

--- app/controllers/forum_posts_controller.rb
+++ app/controllers/forum_posts_controller.rb
@@ -1,15 +1,15 @@
 class ForumPostsController < ApplicationController
 	def create
 	    @forum_topic = ForumTopic.find(params[:forum_topic_id])
-		@forum_post = @forum_topic.forum_posts.build(forum_post_params)
-		@forum_post.save
+        @forum_theme = ForumTheme.find(@forum_topic.forum_theme_id)
+		@forum_post = @forum_topic.forum_posts.create(forum_post_params)
 		if @forum_post.save
 			flash[:success] = "Postagem criada com sucesso."
-			redirect_to forum_topic_path(@forum_topic)
 		else
 			flash[:warning] = "A postagem precisa ter pelo menos 2 caracteres e no máximo 1000."
-			redirect_to forum_topic_path(@forum_topic)
 		end
+		
+        redirect_to forum_theme_forum_topic_path(@forum_theme, @forum_topic)
 	end
 
 	def edit
@@ -20,10 +20,11 @@ class ForumPostsController < ApplicationController
 
 	def update
 		@forum_topic = ForumTopic.find(params[:forum_topic_id])
+        @forum_theme = ForumTheme.find(@forum_topic.forum_theme_id)
     	@forum_post = ForumPost.find(params[:id])
 
 		if @forum_post.update(forum_post_params)
-			redirect_to @forum_topic
+            redirect_to forum_theme_forum_topic_path(@forum_theme, @forum_topic)
 		else
 			render 'edit'
 		end
@@ -31,9 +32,10 @@ class ForumPostsController < ApplicationController
 
 	def destroy
 		@forum_topic = ForumTopic.find(params[:forum_topic_id])
+        @forum_theme = ForumTheme.find(@forum_topic.forum_theme_id)
     	@forum_post = ForumPost.find(params[:id])
 		@forum_post.destroy
-		redirect_to @forum_topic
+		redirect_to forum_theme_forum_topic_path(@forum_theme, @forum_topic)
 	end
 
 private 
@@ -42,11 +44,7 @@ private
 		@forum_post = ForumPost.find(params[:id])
 	end
 
-
-
 	def forum_post_params
-		params.require(:forum_post).permit(:content)
+		params.require(:forum_post).permit(:content, :author)
 	end
-
-
 end
