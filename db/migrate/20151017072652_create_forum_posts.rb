fec80bf4fb8839ec245bcedddd817e7831aae5d9
Luan Guimarães <luan@live.in>
2015-11-29 23:32:52 -0200

    Remove the title and add the author to ForumPosts.

--- db/migrate/20151017072652_create_forum_posts.rb
+++ db/migrate/20151017072652_create_forum_posts.rb
@@ -1,7 +1,7 @@
 class CreateForumPosts < ActiveRecord::Migration
   def change
     create_table :forum_posts do |t|
-      t.string :title
+      t.string :author
       t.text :content
       t.references :forum_topic, index: true
       t.references :forum_theme, index: true
